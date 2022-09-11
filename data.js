511ae6f3ef5b32d9391d37541fd97fc6371f73cf
vignesh <vigneshwaran274mail@gmail.com>
2022-09-11 21:28:56 +0530

    data

---
+++ data.js
@@ -0,0 +1,54 @@
+// number data type
+var vicky=20;
+console.log(vicky);
+
+
+// string data type
+let d="red";
+console.log(d);
+
+let x=20 +"red";
+console.log(x);
+
+let y=10+5+"red"
+console.log(y);
+
+var abc = 'red'+3+25+2;
+console.log(abc);
+
+
+// object
+const person = {
+    fname : "vignesh",
+    lname  : "karthik",
+    age     : 18
+  };
+console.log(person);
+
+
+// array
+const names = ["ajay_bro","sakthi","vicky", "muthu_annov"];
+console.log(names);
+
+
+// boolean
+let a= 10;
+let b = 15;
+let z = 10;
+console.log(a==b);
+console.log(a==z);
+
+// typeof
+console.log(typeof 123);
+console.log(typeof "red");
+
+let m;
+console.log(m);
+
+let l="";
+console.log(l);
+
+// console.log(red);
+
+var cd = 5+'red'+6;
+console.log(cd);
